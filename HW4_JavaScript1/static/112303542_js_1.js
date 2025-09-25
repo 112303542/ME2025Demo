ebac733b42aa47dd70447907752fe25c003d3080
112303542 <112303542@cc.ncu.edu.tw>
2025-09-25 15:06:08 +0800

    112303542_js_1.js

--- HW4_JavaScript1/static/112303542_js_1.js
+++ HW4_JavaScript1/static/112303542_js_1.js
@@ -1,4 +1,20 @@
 function random_answer(min,max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let answer = random_answer(0,100);
+
+var answer = random_answer(0,100);
+var guessCount = 0;
+
+function answer_check(valiable){
+    guessCount += 1;
+    const valiable = document.getElementById('guessNumber');
+    if(valiable == answer){
+        alert('恭喜您猜對了，您一共猜了'+ guessCount +'次。');
+    }
+    else if(valiable < answer){
+        alert('太小了，請再試一次');
+    }
+    else if(valiable > answer){
+        alert('太大了，請再試一次');
+    }
+}
