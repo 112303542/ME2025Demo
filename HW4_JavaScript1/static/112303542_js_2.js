ba9342eaf56b22c1f678668e664ba7a83af6cd96
112303542 <112303542@cc.ncu.edu.tw>
2025-09-26 19:06:15 +0800

    做出數字按鈕

---
+++ HW4_JavaScript1/static/112303542_js_2.js
@@ -0,0 +1,17 @@
+document.writeln('<input type="text" id="display" readonly><br><br>');
+
+let buttons=[
+    ['0','1','2'],
+    ['3','4','5'],
+    ['6','7','8'],
+    ['9','clear'],
+    ['+', '-', '*', '/', '(', ')', '=']
+];
+
+for(let row of buttons){
+    for(let column of row){
+        document.writeln('<button onclick="buttonClick(\'' + column + '\')">' + column + '</button>');
+    }
+    document.writeln('<br>');
+}
+
