bc6d5881164cf399b90e01e8a07e6f08be7a4434
112303542 <112303542@cc.ncu.edu.tw>
2025-10-02 14:26:57 +0800

    計時功能

--- HW5_JavaScript2/static/112303542_js_1.js
+++ HW5_JavaScript2/static/112303542_js_1.js
@@ -4,24 +4,33 @@ function random_answer(min , max){
 
 var answer = random_answer(0 , 100);
 var guessCount = 0;
+var time = 0;
+
+function timer(){
+    time ++ ;
+}
 
 function answer_check(){
+
+    let output = document.getElementById('output');
     guessCount += 1;
     const valiable = parseInt(document.getElementById('guessNumber').value);
 
+    let stop = setInterval(timer,1000);
+
     if(valiable == answer){
-        alert('恭喜您猜對了，您一共猜了'+ guessCount +'次。');
+        alert('恭喜您猜對了，您一共猜了'+ guessCount +'次，' + '一共花了' + stop + '秒。');
         answer = random_answer(0 , 100);
         guessCount = 0;
+        output.innerHTML = '';
+        clearInterval(timer);
     }
 
     else if(valiable < answer){
-        let output = document.getElementById('output');
         output.innerHTML = '太小了，請再試一次';
     }
-    
+
     else if(valiable > answer){
-        let output = document.getElementById('output');
         output.innerHTML = '太大了，請再試一次';
     }
 }
