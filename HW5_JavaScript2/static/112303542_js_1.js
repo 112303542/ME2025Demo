62532fe2082f38e9ce28c87f81656c92e0e1b60e
112303542 <112303542@cc.ncu.edu.tw>
2025-10-02 15:09:48 +0800

    計時功能優化

--- HW5_JavaScript2/static/112303542_js_1.js
+++ HW5_JavaScript2/static/112303542_js_1.js
@@ -4,26 +4,30 @@ function random_answer(min , max){
 
 var answer = random_answer(0 , 100);
 var guessCount = 0;
-var time = 0;
+var time = 0.00;
 
 function timer(){
-    time ++ ;
+    time += 0.01;
+    console.log(time.toFixed(2));
+    document.getElementById('output_2').innerHTML = '遊戲時間' + time.toFixed(2) + '秒';
 }
 
 function answer_check(){
-
+    console.log(answer);
     let output = document.getElementById('output');
     guessCount += 1;
     const valiable = parseInt(document.getElementById('guessNumber').value);
 
-    let stop = setInterval(timer,1000);
+    setInterval(timer,10);
 
     if(valiable == answer){
-        alert('恭喜您猜對了，您一共猜了'+ guessCount +'次，' + '一共花了' + stop + '秒。');
+        alert('恭喜您猜對了，您一共猜了'+ guessCount +'次，' + '一共花了' + time.toFixed(2) + '秒。');
         answer = random_answer(0 , 100);
         guessCount = 0;
         output.innerHTML = '';
         clearInterval(timer);
+        let history = document.createComment;
+        //history.value =  
     }
 
     else if(valiable < answer){
