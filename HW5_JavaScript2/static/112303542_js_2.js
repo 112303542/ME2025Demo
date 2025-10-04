6a190e696b203a1d980700adb3a6a8d793a1ca0b
112303542 <112303542@cc.ncu.edu.tw>
2025-10-05 00:16:14 +0800

    .js

---
+++ HW5_JavaScript2/static/112303542_js_2.js
@@ -0,0 +1,6 @@
+const items = document.querySelectorAll(".item");
+const minusBtns = document.querySelectorAll(".minus");
+const plusBtns = document.querySelectorAll(".plus");
+const totalCell = document.getElementById("total");
+const checkoutBtn = document.getElementById("checkout");
+
