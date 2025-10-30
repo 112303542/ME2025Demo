bc69137110c276f353d563b29569c559a4b985a9
112303542 <112303542@cc.ncu.edu.tw>
2025-10-30 17:09:28 +0800

    404debug

--- EXAM2-backend-main/shopping_system/static/script.js
+++ EXAM2-backend-main/shopping_system/static/script.js
@@ -365,7 +365,7 @@ async function handleLogin(event) {
 
   // 依你後端邏輯處理導向
   if (response.ok) {
-    location.href = '../templates/index.html'; // 例如登入成功返回首頁
+    location.href = '/index'; // 例如登入成功返回首頁
   } else {
     alert('登入失敗');
   }
